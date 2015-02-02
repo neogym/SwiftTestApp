90e59099dcc016a825c124fca0ffae2e232826e7
Masaki Tatematsu <neogym@users.noreply.github.com>
2015-02-02 16:10:32 +0900

    Initial Commit

---
+++ SwiftTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestApp
+//
+//  Created by masaki tatematsu on 2015/02/02.
+//  Copyright (c) 2015年 Masaki Tatematsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
